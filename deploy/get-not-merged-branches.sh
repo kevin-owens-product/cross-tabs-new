9e18aac83823f27ecd73f910e55c09be044dff46
forensor <forensor@hotmail.com>
2025-04-16 23:58:02 +0200

    Added GHA workflows

--- deploy/get-not-merged-branches.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$NOT_MERGED_BRANCHES" ]; then
-    git ls-remote --heads origin | grep -Eo "refs/heads/.*" | sed 's/refs\/heads\///g' | paste -sd "," -
-else
-    echo $NOT_MERGED_BRANCHES
-fi
